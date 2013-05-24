87197789937c80fe85529ca5443cbfb57627a709
Andy Yao <andyaoy@gmail.com>
2013-05-24 18:30:35 +0800

    Update env.

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -100,7 +100,7 @@ alias lk='ll -Sr'        # Lists sorted by size, largest last.
 alias lt='ll -tr'        # Lists sorted by date, most recent last.
 alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
 alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-alias sl='ls'            # I often screw this up.
+#alias sl='ls'            # I often screw this up.
 
 # Mac OS X Everywhere
 if [[ "$OSTYPE" == darwin* ]]; then
